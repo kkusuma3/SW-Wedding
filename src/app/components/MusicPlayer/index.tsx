1ceb1c36440b0f09656fd957aa188e728cd69a30
Kevin Kusuma <kevindwiputrak@gmail.com>
2024-03-13 23:54:57 -0400

    feat: disable autoplay for music player

--- src/app/components/MusicPlayer/index.tsx
+++ src/app/components/MusicPlayer/index.tsx
@@ -1,13 +1,11 @@
 "use client";
 import { MusicSrc } from '@/app/shared/staticImportsSrc';
 import { MdOutlinePlayCircleFilled, MdOutlinePauseCircleFilled } from "react-icons/md";
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import styles from './styles.module.css';
 
-const iconClassName = ""
-
 const MusicPlayer = () => {
-    const [isMusicPlaying, setIsMusicPlaying] = useState(true);
+    const [isMusicPlaying, setIsMusicPlaying] = useState(false);
     const audioRef = useRef<HTMLAudioElement>(null);
 
     const startAudio = () => {
@@ -19,12 +17,11 @@ const MusicPlayer = () => {
         audioRef.current?.pause();
         setIsMusicPlaying(false);
     };
-    
 
     return (        
         <figure className="fixed bottom-5 md:bottom-10 right-5 md:right-10 text-4xl text-secondary">
             { isMusicPlaying ? <MdOutlinePauseCircleFilled className={styles.btn__music_player} onClick={pauseAudio} /> : <MdOutlinePlayCircleFilled className={styles.btn__music_player} onClick={startAudio} /> }
-            <audio ref={audioRef} autoPlay src={MusicSrc}></audio>
+            <audio ref={audioRef} loop src={MusicSrc}></audio>
         </figure>
     )
 };
