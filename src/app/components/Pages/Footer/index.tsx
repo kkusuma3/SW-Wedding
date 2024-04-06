850a86504aab7d0c40aee347b765795a659976da
Kevin Kusuma <kevin.kusuma@revelsystems.com>
2024-04-06 08:59:38 -0700

    feat: fix padding for footer

--- src/app/components/Pages/Footer/index.tsx
+++ src/app/components/Pages/Footer/index.tsx
@@ -7,7 +7,7 @@ import { useEffect } from "react";
 import { ImgFooterSrc } from '@/app/shared/staticImportsSrc';
 
 export const FooterContent = () => (
-    <div className="flex flex-col w-full h-full md:w-2/3 mx-auto text-center text-primary py-10 px-5 justify-between" data-aos="zoom-in-up">
+    <div className="flex flex-col w-full h-full md:w-2/3 mx-auto text-center text-primary pt-20 pb-10 px-5 justify-between" data-aos="zoom-in-up">
         <div className="flex flex-col">
             <span className="text-lg md:text-2xl p-2">See you on our big day</span>
             <span className="text-5xl md:text-8xl p-2 font-italianno">Sebastian & Widyana</span>
